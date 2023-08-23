ef2e44709195724740f26567d512118e89deef2b
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-08-23 20:01:03 +0300

    Creating All files, and renaming convention

---
+++ C_Sameh_Afifi_Tasks/7_2.c
@@ -0,0 +1,7 @@
+/*  -------------------------------------------------------------------
+    Write only the prototype of the function:(2):
+    ---------------------------------------------
+        2. Function that take two variables and return the biggest one. 
+
+        
+*/
---
+++ C_Sameh_Afifi_Tasks/7_3.c
@@ -0,0 +1,7 @@
+/*  -------------------------------------------------------------------
+    Write only the prototype of the function:(3):
+    ---------------------------------------------
+        3. Call back function that take pointer to long and pointer to function (the function return pointer to integer and take char).
+
+        
+*/
