209422b1ae314b6107cf36465d08c054079c5430
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-08-23 16:11:58 +0300

    *** Start Functions Section ***

---
+++ C_Sameh_Afifi_Tasks/3_3.c
@@ -0,0 +1,5 @@
+/*  -------------------------------------------------------------------
+    Functions (3):
+    --------------
+        Write a C Function that checks if an integer is even or odd.
+*/
