6c5dc84bd5738ccb4fb103badc1e1a83733e3d89
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-16 10:34:06 +0300

    Array Max Value Function

--- C_Sameh_Afifi_Tasks/5_4.c
+++
@@ -1,8 +0,0 @@
-/*  -------------------------------------------------------------------
-    Arrays & Pointers (4):
-    ----------------------
-            
-    4. Write a C Function that take an array and its size, return the max number.
-
-        
-*/
--- C_Sameh_Afifi_Tasks/5_4_Array&Pointer_ArrayMaxValue.c
+++ C_Sameh_Afifi_Tasks/5_4_Array&Pointer_ArrayMaxValue.c
@@ -1,10 +1,12 @@
 /*  -------------------------------------------------------------------
-    Arrays & Pointers (3):
+    Arrays & Pointers (4):
     ----------------------
 
-    3. Write a C Function that take an array and its size, then sorting the element descending.
+    4. Write a C Function that take an array and its size, return the max number.
+
 
 */
+
 
 /*  ---------------------------------------------------------------------------
                             Included Libraries:
@@ -15,9 +17,7 @@
                             Functions ProtoType:
     ---------------------------------------------------------------------------*/
 
-void ArraySort_BubbleSort(char *Array, int Length);
-void ArraySort_BubbleSortDescending(char *Array, int Length);
-void ArraySwapElements(char *A1, char *A2);
+char ArrayMaxElement(char *Array, int Length);
 void ArrayPrint(char *Array, int Size);
 
 /*  ---------------------------------------------------------------------------
@@ -37,20 +37,9 @@ int main()
     printf("------\nArray:\n------\n");
     ArrayPrint(array, arraySize);
 
-    // Swap And Print Array 1,2.
-    printf("---------\nSwap 2,3:\n---------\n");
-    ArraySwapElements(&array[2], &array[3]);
-    ArrayPrint(array, arraySize);
-
-    // Print the Array After It's Sorted.
-    printf("-----------\nSorted, Desc:\n-----------\n");
-    ArraySort_BubbleSortDescending(array, arraySize); // Sort
-    ArrayPrint(array, arraySize);                     // Print
-
-    // Print the Array After It's Sorted.
-    printf("------\nSorted:\n------\n");
-    ArraySort_BubbleSort(array, arraySize); // Sort
-    ArrayPrint(array, arraySize);           // Print
+    // Get Max Value inside the Array:
+    char MaxValue = ArrayMaxElement(array, arraySize);
+    printf("Max value = %d\n", MaxValue);
 
     return 0;
 }
@@ -59,39 +48,19 @@ int main()
                             Functions Implementation:
 ---------------------------------------------------------------------------*/
 
-void ArraySort_BubbleSortDescending(char *Array, int Length)
+char ArrayMaxElement(char *Array, int Length)
 {
-    for (int i = 0; i < Length - 1; i++)
+    char Max = 0;
+
+    for (int i = 0; i < Length; i++)
     {
-        for (int j = 0; j < Length - i - 1; j++)
+        if (Array[i] > Max)
         {
-            if (Array[j] < Array[j + 1])
-            {
-                ArraySwapElements(&Array[j], &Array[j + 1]);
-            }
+            Max = Array[i];
         }
     }
-}
 
-void ArraySort_BubbleSort(char *Array, int Length)
-{
-    for (int i = 0; i < Length - 1; i++)
-    {
-        for (int j = 0; j < Length - i - 1; j++)
-        {
-            if (Array[j] > Array[j + 1])
-            {
-                ArraySwapElements(&Array[j], &Array[j + 1]);
-            }
-        }
-    }
-}
-
-void ArraySwapElements(char *A1, char *A2)
-{
-    char Temp = *A1;
-    *A1 = *A2;
-    *A2 = Temp;
+    return Max;
 }
 
 void ArrayPrint(char *Array, int Size)
