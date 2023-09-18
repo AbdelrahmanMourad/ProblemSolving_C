2c622e6ee1f42f4305812eaf1622e328af2977a6
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-19 02:37:03 +0300

    Error With Pointer to Pointer to int

---
+++ C_Sameh_Afifi_Tasks/6_2_Pointers_PointerTo_PointerTo_int.c
@@ -0,0 +1,42 @@
+/*  -------------------------------------------------------------------
+    Just write the below statements:(2):
+    ------------------------------------
+
+        2. Pointer to pointer to int.
+
+
+*/
+
+/*  ---------------------------------------------------------------------------
+                            Included Libraries:
+    ---------------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*  ---------------------------------------------------------------------------
+                            Driving Code:
+    ---------------------------------------------------------------------------*/
+
+int main()
+{
+    // Declare Variable of Type int.
+    int Variable;
+
+    // Declare Pointers to Variable of Type int.
+    int *Ptr1;
+    int *Ptr2;
+
+    // Assign Pointer to Pointer.
+    Ptr1 = Ptr2;
+
+    // Assign Pointer to Variable.
+    Ptr2 = &Variable;
+
+    // Dereference Variable by Pointer to Assign Value inside.
+    *Ptr2 = 5;
+
+    // Print value and Address.
+    printf("&Ptr1 = %x , Ptr1 = %x , *Ptr1 = %x , **Ptr1 = %d\n", &Ptr1, Ptr1, *Ptr1, (**Ptr1));
+    printf("&Ptr2 = %x , Ptr2 = %x , &Var = %x , Var = %d , *Ptr2 = %d\n", &Ptr2, Ptr2, &Variable, Variable, *Ptr2);
+
+    return 0;
+}
