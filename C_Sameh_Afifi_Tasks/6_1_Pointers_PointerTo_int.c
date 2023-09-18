c24345b2989084b26b834606cc3d17f827d46d64
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-19 02:26:34 +0300

    Pointer To Int

--- C_Sameh_Afifi_Tasks/6_1_Pointers_PointerTo_int.c
+++ C_Sameh_Afifi_Tasks/6_1_Pointers_PointerTo_int.c
@@ -1,24 +1,15 @@
 /*  -------------------------------------------------------------------
     Just write the below statements:(1):
     ------------------------------------
-                
+
         1. Pointer to int.
- 
-                
+
 */
-
-
-
 
 /*  ---------------------------------------------------------------------------
                             Included Libraries:
     ---------------------------------------------------------------------------*/
-
-
-/*  ---------------------------------------------------------------------------
-                            Functions ProtoType:
-    ---------------------------------------------------------------------------*/
-
+#include <stdio.h>
 
 /*  ---------------------------------------------------------------------------
                             Driving Code:
@@ -26,11 +17,20 @@
 
 int main()
 {
+    // Declare Variable of Type int.
+    int Variable;
+
+    // Declare Pointer to Variable of Type int.
+    int *Ptr;
+
+    // Assign Pointer to Variable.
+    Ptr = &Variable;
+
+    // Dereference Variable by Pointer to Assign Value inside.
+    *Ptr = 5;
+
+    // Print value and Address.
+    printf("&Ptr = %x , Ptr = %x , &Var = %x , Var = %d , *Ptr = %d\n", &Ptr, Ptr, &Variable, Variable, *Ptr);
 
     return 0;
 }
-
-/*  ---------------------------------------------------------------------------
-                            Functions Implementation:
----------------------------------------------------------------------------*/
-
