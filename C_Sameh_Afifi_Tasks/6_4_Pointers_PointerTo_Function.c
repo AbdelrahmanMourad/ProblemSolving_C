d367bebbd0a97c555eef8085158b66f5c593bb11
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-30 23:15:22 +0300

    Pointer To Function

--- C_Sameh_Afifi_Tasks/6_4_Pointers_PointerTo_Function.c
+++ C_Sameh_Afifi_Tasks/6_4_Pointers_PointerTo_Function.c
@@ -1,24 +1,21 @@
 /*  -------------------------------------------------------------------
     Just write the below statements:(4):
     ------------------------------------
-        
+
         4. Pointer to function which take 2 int and return void.
-       
-                
+
+
 */
-
-
-
 
 /*  ---------------------------------------------------------------------------
                             Included Libraries:
     ---------------------------------------------------------------------------*/
-
+#include <stdio.h>
 
 /*  ---------------------------------------------------------------------------
                             Functions ProtoType:
     ---------------------------------------------------------------------------*/
-
+void FunctionExample(int a, int b);
 
 /*  ---------------------------------------------------------------------------
                             Driving Code:
@@ -26,6 +23,14 @@
 
 int main()
 {
+    // Declare Pointer To Function.
+    void (*PointerToFunction)(int, int); //  <-------------------
+
+    // Assign Pointer To Function. {Function Name is Address Of First Line Of it}.
+    PointerToFunction = FunctionExample;
+
+    // Call The Function Through The pointer.
+    PointerToFunction(1, 2);
 
     return 0;
 }
@@ -33,4 +38,7 @@ int main()
 /*  ---------------------------------------------------------------------------
                             Functions Implementation:
 ---------------------------------------------------------------------------*/
-
+void FunctionExample(int a, int b)
+{
+    printf("Example Function Called by Pointer.. Sum = %d", a + b);
+}
