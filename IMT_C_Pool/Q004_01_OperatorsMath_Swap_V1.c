e4c019ae1ab3f00d887e0d113eb85d82150f6b3b
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-11-06 23:41:22 +0200

    Q004 Swapping_V1

--- IMT_C_Pool/Q004_01_OperatorsMath_Swap_V1.c
+++ IMT_C_Pool/Q004_01_OperatorsMath_Swap_V1.c
@@ -14,7 +14,7 @@
 
                     Write a c code that will ask the user to enter two values
                     and swap then print it
-                    
+
                     Test Case:
                         Input:
                             Enter First Number: 5
@@ -32,5 +32,22 @@
 // Driving Code.
 void main(void)
 {
-   
+    // Declare 3 Variables.
+    int a, b, Temp;
+
+    // Take Input Values From User.
+    printf("Enter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+
+    // Operations.
+    Temp = a;
+    a    = b;
+    b    = Temp;
+
+    // Display Outputs To User.
+    printf("......After Swapping......\n");
+    printf("FirstNumber=%d\n", a);
+    printf("SecondNumber=%d\n", b);
 }
