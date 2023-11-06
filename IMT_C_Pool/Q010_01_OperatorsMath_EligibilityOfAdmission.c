d981e80707ee464227c33b3aebaad234c02bddcf
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-11-07 01:44:51 +0200

    Q010 Eligibility For Admission

--- IMT_C_Pool/Q010_01_OperatorsMath_EligibilityOfAdmission.c
+++ IMT_C_Pool/Q010_01_OperatorsMath_EligibilityOfAdmission.c
@@ -8,17 +8,17 @@
             Section(1): Mathematical Operators:
             -----------------------------------
 
-                ---------------------
-                Problem (002): ASCII:
-                ---------------------
+                ----------------------------------------
+                Problem (010): Eligibility Of Admission:
+                ----------------------------------------
 
                     Write a C program to find the eligibility of admission for
                     a professional course based on the following criteria:
-                        Marks in Maths              >=  65 
-                        Marks in Phy                >=  55 
-                        Marks in Chem               >=  50 
-                        Total in all three subject  >=  180 
-                        Or  
+                        Marks in Maths              >=  65
+                        Marks in Phy                >=  55
+                        Marks in Chem               >=  50
+                        Total in all three subject  >=  180
+                        Or
                         Total in Math and Subjects  >=  140
 
                     Test Case:
@@ -35,7 +35,38 @@
 #include <stdio.h>
 
 // Driving Code.
-void main(void)
+int main()
 {
-    
+    // Declare Variables.
+    int M_Maths, M_Phy, M_Chem, M_Total;
+
+    // Take Inputs From User.
+    printf("Enter the degree of course one: ");
+    scanf("%d", &M_Maths);
+    printf("Enter the degree of course two: ");
+    scanf("%d", &M_Phy);
+    printf("Enter the degree of course three: ");
+    scanf("%d", &M_Chem);
+
+    // Algorithm.
+    M_Total = M_Maths + M_Phy + M_Chem;
+
+    if (M_Maths >= 65 && M_Phy >= 55 && M_Chem >= 50)
+    {
+        printf("The Candidate Is Eligible For Admission - Rating(1)");
+    }
+    if (M_Total >= 180)
+    {
+        printf("The Candidate Is Eligible For Admission - Rating(2)");
+    }
+    else if (M_Maths >= 65 && M_Total >= 140)
+    {
+        printf("The Candidate Is Eligible For Admission - Rating(3)");
+    }
+    else
+    {
+        printf("The Candidate Is Not Eligible For Admission");
+    }
+
+    return 0;
 }
